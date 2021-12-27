9e5138eb8dd113425902f9534d33100f1174aaca
Jeong-heon2 <cjhgg2@gmail.com>
2021-12-27 20:41:48 +0900

    PG 영어 끝말잇기 - hash

---
+++ java/src/hash/PG_영어_끝말잇기.java
@@ -0,0 +1,24 @@
+package hash;
+import java.util.*;
+
+public class PG_영어_끝말잇기 {
+
+    class Solution {
+        public int[] solution(int n, String[] words) {
+            HashSet<String> set = new HashSet<>();
+            set.add(words[0]);
+            int round = 1;
+            for (int i = 1 ; i < words.length ; i++) {
+                int num = i % n + 1;
+                if (set.contains(words[i]) ||
+                        words[i-1].charAt(words[i-1].length() - 1) != words[i].charAt(0)
+                ) {
+                    return new int[] {num, round};
+                }
+                set.add(words[i]);
+                if (num == n) round++;
+            }
+            return new int[] {0, 0};
+        }
+    }
+}
