27826b5359a14263fe5232d0a7c879a5c114354b
Jeong-heon2 <cjhgg2@gmail.com>
2022-06-10 09:43:34 +0900

    Leet - stack

--- java/src/stack/LEET_Longest_Valid_Parentheses.java
+++ java/src/stack/LEET_Longest_Valid_Parentheses.java
@@ -6,8 +6,9 @@ import java.util.Stack;
 public class LEET_Longest_Valid_Parentheses {
 
     public static void main(String... args) {
-        Solution s = new Solution();
-        System.out.println(s.longestValidParentheses(")()())"));
+//        Solution s = new Solution();
+//        System.out.println(s.longestValidParentheses(")()())"));
+        longestValidParentheses("())(())");
     }
     static class Solution {
         public int longestValidParentheses(String s) {
@@ -76,4 +77,23 @@ public class LEET_Longest_Valid_Parentheses {
         }
     }
 
+    // stack 을 이용한 O(n) 해법
+    public static int longestValidParentheses(String s) {
+        int maxans = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.empty()) {
+                    stack.push(i);
+                } else {
+                    maxans = Math.max(maxans, i - stack.peek());
+                }
+            }
+        }
+        return maxans;
+    }
 }
